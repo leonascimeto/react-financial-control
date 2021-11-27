e54bb23242d522365467bc20045c72d331e14160
Leo Nascimento <31623733+leonascimeto@users.noreply.github.com>
2021-11-27 13:29:07 -0300

    layout grid

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
+
+import DashBoard from './pages/Dashboard';
+import Layout from './components/Layout';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Layout />
+    </>
+  )
+}
+
+export default App;
